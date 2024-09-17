a9e587c9a890d2a96772bfe237aa5fd3ac3af113
Tamim <jobaertamim7@gmail.com>
2024-09-18 02:25:51 +0600

    Data handling fixed

--- src/data_handle.py
+++ src/data_handle.py
@@ -1,5 +1,5 @@
 import json
-from os.path import join,dirname
+from os.path import join,dirname,exists
 
 data_path = join(dirname(__file__),"..","data.json")
 
@@ -9,14 +9,15 @@ def dump_data_to_json(data, file_path=data_path):
         json.dump(data, json_file, indent=4)
 
 def load_data_from_json(file_path=data_path):
-        if file_path is not None:   
+        if not exists(file_path):  
+            with open(file_path,'w') as json_file:
+                json.dump({},json_file,indent=4)
+                return {} 
+        else:
             with open(file_path, 'r+') as json_file:
                 data = json.load(json_file)
                 return data
-        else:
-            with open(file_path,'w') as json_file:
-                json.dump({},json_file,indent=4)
-                return {}
+
         
 
 
